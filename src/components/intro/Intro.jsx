b2d0cc04a5f8181fdd821c539273b65054ab92fc
Muhammad Afnan Abbasi <abbasiafnan9@gmail.com>
2021-12-22 16:40:09 -0800

    initial commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Intro() {
+    return (
+        <div className="i">
+            <div className="i-left"></div>
+            <div className="i-right"></div>
+        </div>
+    )
+}
+
+
